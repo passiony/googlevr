136ba77326ee1c302ef5500420cb667b6d793c48
passion <563129582@qq.com>
2023-05-26 19:59:43 +0800

    提交代码

--- Assets/Scripts/GazeButton.cs
+++ Assets/Scripts/GazeButton.cs
@@ -1,19 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GazeButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     public void OnPointerEnter()
     {
-        SceneManager.LoadScene("Gaze");
+        SceneManager.LoadScene("Gaze2");
     }
 }
